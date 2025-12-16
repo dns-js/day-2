d51a999844a9d5868678faa85b34e678ba839769
Theodore Raphaello <theodoregiovannitheo@gmail.com>
2025-12-16 21:19:10 +0700

    fix: add a error handling in case the data is empty and update the function save task with no adding new task to the database

--- Day2/src/service/taskStore.js
+++ Day2/src/service/taskStore.js
@@ -1,29 +1,19 @@
 import { readFile, writeFile } from "fs/promises"
 
+const filePath = '../../data/tasks.json'
 
-const data = {
-    "id": "t_1001",
-    "title": "Menyelesaikan laporan akhir tahun",
-    "status": "open",
-    "priority": "high",
-    "due": "2024-12-31",
-    "createdAt": "2024-10-01T09:00:00.000Z",
-    "updatedAt": "2024-10-05T14:30:00.000Z"
-  }
-
-
-async function loadTask() {
-    const response = await readFile('../../data/tasks.json')
-    const data = JSON.parse(response)
-    return data
+export async function loadTask() {
+    try {
+        const response = await readFile(filePath)
+        return JSON.parse(response)    
+    } catch (error) {
+        return [];
+    }
 }
 
-async function saveTask(data) {
-    const datas = await loadTask()
-    datas.push(data)
-    const updatedData = JSON.stringify(datas, null, 2)
+export async function saveTask(tasks) {
+    const updatedData = JSON.stringify(tasks, null, 2)
     await writeFile('../../data/tasks.json', updatedData)
 }
 
-export default { loadTask, saveTask }
-saveTask(data)
+
