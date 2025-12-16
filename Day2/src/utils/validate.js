da62a7c5d4fae8b5e4b9017aa3e91522bc39e08a
Theodore Raphaello <theodoregiovannitheo@gmail.com>
2025-12-16 20:59:32 +0700

    chore: add a validation process to compare due date with date today

---
+++ Day2/src/utils/validate.js
@@ -0,0 +1,40 @@
+
+
+function validateAllTasks(tasksArray) {
+
+    const validPriorities = ["low", "medium", "high"];
+
+    tasksArray.forEach((task) => {
+        if (!task.title || typeof task.title !== "string" || task.title.trim().length < 3) {
+            throw new Error(`Title diperlukan, harus string, dan minimal 3 karakter.`);
+        }
+        if (task.priority && !validPriorities.includes(task.priority)) {
+            throw new Error(`Priority harus low, medium, atau high.`);
+        }
+        if (task.due !== null) {
+            if (typeof task.due !== "string" || !/^\d{4}-\d{2}-\d{2}$/.test(task.due)) {
+                throw new Error(`Due date harus berupa null atau string YYYY-MM-DD.`);
+            }
+            
+            const dateObj = new Date(task.due);
+            if (isNaN(dateObj.getTime())) {
+                 throw new Error(`Due date telah berlalu.`);
+            }
+        }
+    });
+    return tasksArray; 
+}
+
+const inputData = [
+  { id: "t_1001", title: "Laporan Final", priority: "high", due: "2024-12-31" }, 
+  { id: "t_1002", title: "Meeting Cepat", priority: "medium", due: null },      
+  { id: "t_1003", title: "OK", priority: "low", due: "2024-11-20" },             // <-- GAGAL DI SINI (Title < 3)
+];
+
+try {
+    const validatedData = validateAllTasks(inputData);
+    console.log("Semua data valid.");
+    
+} catch (error) {
+    console.error("VALIDASI GAGAL:", error.message);
+}
